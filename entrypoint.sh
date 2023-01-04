628b3f68ab8826ac0546752a2ca44a21d80a69e4
TeoVincent <artur.wincenciak@gmail.com>
2023-01-05 00:49:30 +0100

    Fixes and adjust the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh -l
 
 # Exit codes
 SUCCESS=0
@@ -8,51 +8,40 @@ EXIT_WITH_FAST_FAIL=2
 echo ""
 echo "--- --- ---"
 echo "Alright GitHub Action Cleanup Code Command-Line Tool"
-echo "Default settings:"
+echo "Your setup:"
 echo "- Fail on re-format needed: '$INPUT_FAIL_ON_REFORMAT_NEEDED'"
-echo "- Auto commit re-formatted code: '$AUTO_COMMIT'"
-echo "- ReSharper CLI CleanupCode arguments: '$INPUT_JB_CLEANUPCODE_ARG'"
+echo "- Auto commit re-formatted code: '$INPUT_AUTO_COMMIT'"
+echo "- ReSharper CLI CleanupCode arguments: '$INPUT_JB_CLEANUP_CODE_ARG'"
 echo "- Commit message: '$INPUT_COMMIT_MESSAGE'"
 echo "- Commit creator (git user) e-mail: '$INPUT_COMMIT_CREATOR_EMAIL'"
 echo "- Commit creator (git user) name: '$INPUT_COMMIT_CREATOR_NAME'"
-echo "--- --- ---"
-echo ""
-
-if [ "$INPUT_FAIL_ON_REFORMAT_NEEDED" != "yes" ] && [ "$INPUT_FAIL_ON_REFORMAT_NEEDED" != "no" ]
-then
-    echo ""
-    echo "--- --- ---"
-    echo "INVALID ARGUMENT OF '-f' equals '$INPUT_FAIL_ON_REFORMAT_NEEDED'"
-    echo "Set 'yes' or 'no' or omit to use default equals 'no'"
-    echo "--- --- ---"
-    echo ""
-    exit $INVALID_ARGUMENT_ERROR
-fi
-
-if [ "$AUTO_COMMIT" != "yes" ] && [ "$INPUT_AUTO_COMMIT" != "no" ]
-then
-    echo ""
-    echo "--- --- ---"
-    echo "INVALID ARGUMENT OF '-a' equals '$INPUT_AUTO_COMMIT'"
-    echo "Set 'yes' or 'no' or omit to use default equals 'no'"
-    echo "--- --- ---"
-    echo ""
-    exit $INVALID_ARGUMENT_ERROR
-fi
-
-echo ""
-echo "--- --- ---"
-echo "Your setup:"
-echo "- fail on re-format needed: '$INPUT_FAIL_ON_REFORMAT_NEEDED'"
-echo "- auto commit re-formatted code: '$INPUT_AUTO_COMMIT'"
-if [ "$INPUT_FAIL_ON_REFORMAT_NEEDED" = "yes" ] && [ "$INPUT_AUTO_COMMIT" = "yes" ]
-then
-    echo "NOTICE: you have set that the execution will fast fail on re-format needed"
-    echo "NOTICE: auto commit will not be executed because the execution will terminate with fail when re-format is needed"
-    echo "NOTICE: if you want to auto commit execute call the script with '-f no -a yes' arguments"
+if [ "$INPUT_FAIL_ON_REFORMAT_NEEDED" = "yes" ] && [ "$INPUT_AUTO_COMMIT" = "yes" ]; then
+  echo "NOTICE: you have set that the execution will fast fail on re-format needed"
+  echo "NOTICE: auto commit will not be executed because the execution will terminate with fail when re-format is needed"
+  echo "NOTICE: if you want to auto commit execute call the script with '-f no -a yes' arguments"
 fi
 echo "--- --- ---"
 echo ""
+
+if [ "$INPUT_FAIL_ON_REFORMAT_NEEDED" != "yes" ] && [ "$INPUT_FAIL_ON_REFORMAT_NEEDED" != "no" ]; then
+  echo ""
+  echo "--- --- ---"
+  echo "INVALID ARGUMENT OF '-f' equals '$INPUT_FAIL_ON_REFORMAT_NEEDED'"
+  echo "Set 'yes' or 'no' or omit to use default equals 'no'"
+  echo "--- --- ---"
+  echo ""
+  exit $INVALID_ARGUMENT_ERROR
+fi
+
+if [ "$AUTO_COMMIT" != "yes" ] && [ "$INPUT_AUTO_COMMIT" != "no" ]; then
+  echo ""
+  echo "--- --- ---"
+  echo "INVALID ARGUMENT OF '-a' equals '$INPUT_AUTO_COMMIT'"
+  echo "Set 'yes' or 'no' or omit to use default equals 'no'"
+  echo "--- --- ---"
+  echo ""
+  exit $INVALID_ARGUMENT_ERROR
+fi
 
 echo ""
 echo "--- --- ---"
@@ -66,34 +55,31 @@ jb cleanupcode Blef.sln "$INPUT_JB_CLEANUP_CODE_ARG"
 
 REFORMATTED_FILES=$(git diff --name-only)
 
-if [ -z "$REFORMATTED_FILES" ]
-then
-    echo ""
-    echo "--- --- ---"
-    echo "No files re-formatted, everything is clean, congratulation!"
-    echo "--- --- ---"
-    echo ""
-    exit $SUCCESS
+if [ -z "$REFORMATTED_FILES" ]; then
+  echo ""
+  echo "--- --- ---"
+  echo "No files re-formatted, everything is clean, congratulation!"
+  echo "--- --- ---"
+  echo ""
+  exit $SUCCESS
 fi
 
-if [ "$INPUT_FAIL_ON_REFORMAT_NEEDED" = "yes" ]
-then
-    echo ""
-    echo "--- --- ---"
-    echo "Exit with re-formatted code needed fail status"
-    echo "--- --- ---"
-    echo ""
-    exit $EXIT_WITH_FAST_FAIL
+if [ "$INPUT_FAIL_ON_REFORMAT_NEEDED" = "yes" ]; then
+  echo ""
+  echo "--- --- ---"
+  echo "Exit with re-formatted code needed fail status"
+  echo "--- --- ---"
+  echo ""
+  exit $EXIT_WITH_FAST_FAIL
 fi
 
-if [ "$INPUT_AUTO_COMMIT" = "no" ]
-then
-    echo ""
-    echo "--- --- ---"
-    echo "There is re-formatted code but it will not be auto committed"
-    echo "--- --- ---"
-    echo ""
-    exit $SUCCESS
+if [ "$INPUT_AUTO_COMMIT" = "no" ]; then
+  echo ""
+  echo "--- --- ---"
+  echo "There is re-formatted code but it will not be auto committed"
+  echo "--- --- ---"
+  echo ""
+  exit $SUCCESS
 fi
 
 echo ""
